bcf34947b4e02c9871429945dd14dafdd8d8930f
bdinesh <bdinesh@s3groupinc.com>
2019-12-05 15:53:09 +0530

    Understood thymeleaf

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
+    @GetMapping(value={"/", "/home"})
     public String test() {
         return "index";
     }

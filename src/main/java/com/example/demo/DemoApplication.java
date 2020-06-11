f8d802b5823ff9b117a5a51c3bdc8974d3bc52e2
bdinesh <bdinesh@s3groupinc.com>
2020-06-11 12:36:31 +0530

    Working perfectly...

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 import java.util.Map;
 
@@ -48,8 +55,24 @@ public class DemoApplication {
 
   @GetMapping("/user")
   public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-    String login = principal.getAttribute("login");
-    return Collections.singletonMap("name", principal.getAttribute("login"));
+    String name = principal.getAttribute("name");
+    return Collections.singletonMap("name", name);
+  }
+
+  @GetMapping("/error")
+  public String error(HttpServletRequest request) {
+    String message = (String) request.getSession().getAttribute("error.message");
+    request.getSession().removeAttribute("error.message");
+    return message;
+  }
+
+  @RequestMapping(value = "/logout", method = RequestMethod.GET)
+  public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null) {
+      new SecurityContextLogoutHandler().logout(request, response, auth);
+    }
+    return "redirect:/login";
   }
 
   public static void main(String[] args) {
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,25 +1,26 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-/*@EnableWebSecurity
-@PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)*/
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-/*
-  @Autowired
-  private Environment env;
-*/
+  @Bean
+  public AuthenticationFailureHandler customAuthenticationFailureHandler() {
+    return new CustomAuthenticationFailureHandler();
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests(a -> a
-            .antMatchers("/", "/css/**", "/images/**", "/logout", "/error", "/webjars/**").permitAll()
+            .antMatchers("/", "/css/**", "/images/**", "/error", "/webjars/**").permitAll()
             .anyRequest().authenticated()
         )
         .exceptionHandling(e -> e
@@ -28,85 +29,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .oauth2Login()
         .and()
         .logout(l -> l
-            .logoutSuccessUrl("/").permitAll()
+            .logoutSuccessUrl("/"))
+        .csrf(c -> c
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .oauth2Login(o -> o
+            .failureHandler(customAuthenticationFailureHandler())
         );
-    /*
-        .loginPage("/login")
-        .authorizationEndpoint()
-        .baseUri("/oauth2/authorize")
-        .authorizationRequestRepository(authorizationRequestRepository())
-        .and()
-        .redirectionEndpoint()
-        .baseUri("http://localhost:8080/loginSuccess")
-        .and()
-        .tokenEndpoint()
-        .accessTokenResponseClient(accessTokenResponseClient())
-        .and()
-        .defaultSuccessUrl("/loginSuccess", true)
-        .failureUrl("/loginSuccess");
-*/
 //    http.csrf().disable();
   }
-
-/*
-  @Bean
-  public PrincipalExtractor githubPrincipalExtractor() {
-    return new GithubPrincipalExtractor();
-  }
-
-  @Bean
-  public AuthoritiesExtractor githubAuthoritiesExtractor() {
-    return new GithubAuthoritiesExtractor();
-  }
-*/
-/*
-  @Bean
-  public AuthorizationRequestRepository<OAuth2AuthorizationRequest>
-  authorizationRequestRepository() {
-    return new HttpSessionOAuth2AuthorizationRequestRepository();
-  }
-
-  @Bean
-  public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>
-  accessTokenResponseClient() {
-    DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient = new DefaultAuthorizationCodeTokenResponseClient();
-    return accessTokenResponseClient;
-  }
-
-  private static List<String> clients = Arrays.asList("google", "facebook");
-
-  public ClientRegistrationRepository clientRegistrationRepository() {
-    List<ClientRegistration> registrations = clients.stream()
-        .map(c -> getRegistration(c))
-        .filter(registration -> registration != null)
-        .collect(Collectors.toList());
-
-    return new InMemoryClientRegistrationRepository(registrations);
-  }
-
-  private static String CLIENT_PROPERTY_KEY
-      = "spring.security.oauth2.client.registration.";
-
-  private ClientRegistration getRegistration(String client) {
-    String clientId = env.getProperty(
-        CLIENT_PROPERTY_KEY + client + ".client-id");
-
-    if (clientId == null) {
-      return null;
-    }
-
-    String clientSecret = env.getProperty(
-        CLIENT_PROPERTY_KEY + client + ":client-secret");
-
-    if (client.equals("google")) {
-      return CommonOAuth2Provider.GOOGLE.getBuilder(client)
-          .clientId(clientId).clientSecret(clientSecret).build();
-    }
-    if (client.equals("facebook")) {
-      return CommonOAuth2Provider.FACEBOOK.getBuilder(client)
-          .clientId(clientId).clientSecret(clientSecret).build();
-    }
-    return null;
-  }
-*/
 }
